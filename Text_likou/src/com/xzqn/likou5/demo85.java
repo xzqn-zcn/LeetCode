654edab4e2f6c47ed1a1a173e0614006ca2c2503
xzqn <Love__w@hotmail.com>
2023-06-07 20:55:42 +0800

    增加标题

--- Text_likou/src/com/xzqn/likou5/demo85.java
+++ Text_likou/src/com/xzqn/likou5/demo85.java
@@ -2,7 +2,7 @@ package com.xzqn.likou5;
 
 public class demo85 {
     /**
-     * 盛最多水的容器
+     * 11. 盛最多水的容器
      * 给定一个长度为 n 的整数数组height。有n条垂线，第 i 条线的两个端点是(i, 0)和(i, height[i])。
      * <p>
      * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
--- Text_likou/src/com/xzqn/likou5/demo91.java
+++ Text_likou/src/com/xzqn/likou5/demo91.java
@@ -1,6 +1,11 @@
 package com.xzqn.likou5;
 
 public class demo91 {
+    /**
+     * 204. 计数质数
+     * 给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。
+     * @param args
+     */
     public static void main(String[] args) {
         int n = 500000;//25
         //埃氏筛算法
